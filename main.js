e847b490070a41ce9621c71816d0c438c790ccfe
MiaVilke <44716098+MiaVilke@users.noreply.github.com>
2025-04-25 07:26:37 -0700

    c

--- main.js
+++ main.js
@@ -10,6 +10,8 @@ bgLoader.load('./assets/background.png', (texture) => {
   scene.background = texture;
 });
 
+scene.background = new THREE.Color(0x202020);
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 camera.position.set(0, 1.5, 3);
