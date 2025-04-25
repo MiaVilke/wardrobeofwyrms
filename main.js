107e8bef958fc91c9ded41733aa741974238feb6
MiaVilke <44716098+MiaVilke@users.noreply.github.com>
2025-04-25 07:25:12 -0700

    m

--- main.js
+++ main.js
@@ -3,6 +3,13 @@ import * as THREE from './libs/three.module.js';
 import { OrbitControls } from './libs/OrbitControls.js';
 import { GLTFLoader } from './libs/GLTFLoader.js';
 
+import { TextureLoader } from 'https://unpkg.com/three@0.160.0/build/three.module.js';
+
+const bgLoader = new TextureLoader();
+bgLoader.load('./assets/background.png', (texture) => {
+  scene.background = texture;
+});
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 camera.position.set(0, 1.5, 3);
